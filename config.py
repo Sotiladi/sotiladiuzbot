a955dfda4bcde6c94c94f3bcc3f3f77f5b7a7215
ViktorinaUzBot <5104440@mail.ru>
2016-11-17 09:18:31 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 # Этот токен невалидный, можете даже не пробовать :)
-token = '274003719:AAFPoA7EHJMBKk62CoQarYraONHPCsYca50'
+token = '165100366:AAGryCG7UBGave4NC_HogrHmUy5oXhuMr3A'
